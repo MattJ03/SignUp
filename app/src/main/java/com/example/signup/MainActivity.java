4f22039679d26f780c526a272561e5bec9ac246e
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-13 20:50:16 +0100

    added the drawable to the top of screen and added core ktx dependency

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -39,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
                Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
            }
            }
+
+
             });
     }
 }
