135e91a25ef2aac77c24c2f9fa341510cda73a0e
Matt Shaw <Shaw-M18@ulster.ac.uk>
2025-04-14 20:54:45 +0100

    created the channel

--- app/src/main/java/com/example/signup/MainActivity.java
+++ app/src/main/java/com/example/signup/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.signup;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -26,13 +29,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         usernameInput = findViewById(R.id.editTextTextEmailAddress);
         passwordInput = findViewById(R.id.editTextTextPassword);
         buttonSub = findViewById(R.id.submitBtn);
 
-        String notifTitle = "Sign Up attempt";
-        String failText = "Login credentials are wrong";
-        String successText = "Login successful";
+        private void createNotificationChannel() {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                CharSequence name = getString(R.string.channel_name);
+                String description = getString(R.string.channel_description);
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            }
+        }
 
 
         buttonSub.setOnClickListener(new View.OnClickListener() {
